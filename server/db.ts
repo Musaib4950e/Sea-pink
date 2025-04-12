add8e5c02aa41d75f7033a995abf88870f3aa07f
Musaib4950e <howisitgoing097@gmail.com>
2025-04-12 12:33:39 +0530

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,15 +1,14 @@
+
+
+```
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
 import * as schema from "@shared/schema";
 
 neonConfig.webSocketConstructor = ws;
+const dbUrl = 'postgresql://neondb_owner:npg_WgQ8t0GRSFKB@ep-cold-feather-a52q8hd5-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require';
 
-if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
-  );
-}
-
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const pool = new Pool({ connectionString: dbUrl });
 export const db = drizzle({ client: pool, schema });
+```
