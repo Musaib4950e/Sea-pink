4ddbe9ef1faf1cc656ddc19cde834fa54f21f045
Musaib4950e <howisitgoing097@gmail.com>
2025-04-12 12:23:29 +0530

    Add files via upload

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import GitOperationGuide from "@/components/GitOperationGuide";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4 md:p-8">
+      <GitOperationGuide />
+    </div>
+  );
+}
